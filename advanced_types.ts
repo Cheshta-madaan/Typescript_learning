1d2bb755b0f0b4015e87d5d0449202767f65e9dd
Cheshta-madaan <96044893+Cheshta-madaan@users.noreply.github.com>
2022-01-04 12:41:12 +0530

    Update advanced_types.ts

--- advanced_types.ts
+++ advanced_types.ts
@@ -13,3 +13,30 @@ const a1: adminemployee={
     privileges: ["Access to DB"],
     age:30
 }
+
+// discriminating union types and type guards
+interface Bird {
+    type :"bird";
+    flyingSpeed : number;
+}
+
+interface Horse {
+    type: "horse";
+    runningSpeed: number;
+}
+
+type animal= Bird | Horse;
+
+function movingSpeed(animal: animal){  //input animal of type animal
+    let speed :number;
+    switch(animal.type){
+    case "bird":
+        speed= animal.flyingSpeed;
+        break;
+    case "horse":
+        speed= animal.runningSpeed;
+    }
+    console.log("Moving Speed is: "+ speed);   
+}
+
+movingSpeed({ type: "bird", flyingSpeed:20});
